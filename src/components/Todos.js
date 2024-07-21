e2c54f5ba93ca8f9a7f1bb24e82f0452211ec37d
nishant <nishant1952k@gmail.com>
2024-07-21 23:02:28 +0530

    added todos with completed state

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,75 +1,166 @@
 import React, { useEffect, useState } from 'react';
-import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import {
+	getFirestore,
+	collection,
+	getDocs,
+	doc,
+	updateDoc,
+} from 'firebase/firestore';
 import Spinner from './Spinner';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { format } from 'date-fns';
 
 const Todos = () => {
-  const [habits, setHabits] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [selectedDate, setSelectedDate] = useState(new Date());
-  const db = getFirestore();
-  
-  const fetchHabits = async (date) => {
-      const habitsCollection = collection(db, 'habits');
-      const habitsSnapshot = await getDocs(habitsCollection);
-      const habitsList = habitsSnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-      const dayOfWeek = format(date, 'EEEE'); // Get day of the week in full name
-      const filteredHabits = habitsList.filter(habit => habit.daysOfWeek.includes(dayOfWeek));
-      setHabits(filteredHabits.map(habit => {
-        return {
-          ...habit,
-          style: {
-            borderRadius: '10px',
-            backgroundColor: habit.completed ? 'gray' : 'white',
-          }
-        };
-      }));
-      setLoading(false);
-  };
+	const [habits, setHabits] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [selectedDate, setSelectedDate] = useState(new Date());
+	const db = getFirestore();
 
-  useEffect(() => {
-      fetchHabits(selectedDate);
-  }, [selectedDate]); // Fetch habits whenever the selected date changes
-  return (
-    <div className="container mx-auto p-4">
-      <DatePicker
-        selected={selectedDate}
-        onChange={(date) => {
-          setLoading(true);
-          setSelectedDate(date);
-        }}
-        inline
-        className="mb-4"
-      />
-      {loading ? (
-        <Spinner />
-      ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {habits.map((habit) => (
-            <div key={habit.id} className={`bg-white shadow-md rounded-lg p-4 mb-4 ${habit.style}`}>
-              <h3 className={habit.completed ? "text-green-500 line-through" : "text-gray-800"}>
-                {habit.name}
-              </h3>
-              <div className="mt-4 flex justify-center space-x-2">
-                {habit.daysOfWeek.map((day, index) => (
-                  <div
-                    key={index}
-                    className="h-8 w-8 flex items-center justify-center bg-gray-200 rounded-full"
-                  >
-                    {day.slice(0, 1).toUpperCase()}
-                  </div>
-                ))}
-              </div>
-              <input type="checkbox" checked={habit.completed} className="form-checkbox h-5 w-5 mt-4" readOnly />
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );  
-};  
+	// Function to update habit history in Firestore and local state
+	const updateHabitHistory = async (habitId, isCompleted) => {
+		try {
+			const habitRef = doc(db, 'habits', habitId);
+			const today = format(selectedDate, 'yyyy-MM-dd');
+			const action = isCompleted ? 'finished' : 'didNotAttempt';
 
+			// Update the history for the selected date in Firestore
+			await updateDoc(habitRef, {
+				[`history.${today}`]: { actionPerformed: action },
+			});
+
+			// Update local state for immediate UI feedback
+			setHabits((prevHabits) => {
+				return prevHabits.map((habit) => {
+					if (habit.id === habitId) {
+						return {
+							...habit,
+							completed: isCompleted,
+							style: {
+								borderRadius: '10px',
+								backgroundColor: isCompleted ? 'gray' : 'white',
+							},
+						};
+					}
+					return habit;
+				});
+			});
+		} catch (error) {
+			console.error('Error updating habit history:', error);
+		}
+	};
+
+	const handleCheckboxChange = async (habitId) => {
+		// Find the habit to update
+		const habitToUpdate = habits.find((habit) => habit.id === habitId);
+		if (!habitToUpdate) return;
+
+		const newCompleted = !habitToUpdate.completed;
+
+		// Update habit history in Firestore and local state
+		await updateHabitHistory(habitId, newCompleted);
+	};
+
+	const fetchHabits = async (date) => {
+		setLoading(true);
+		const habitsCollection = collection(db, 'habits');
+		const habitsSnapshot = await getDocs(habitsCollection);
+		const habitsList = habitsSnapshot.docs.map((doc) => ({
+			id: doc.id,
+			...doc.data(),
+		}));
+		const dayOfWeek = format(date, 'EEEE'); // Get day of the week in full name
+
+		const filteredHabits = habitsList.filter((habit) =>
+			habit.daysOfWeek.includes(dayOfWeek)
+		);
+		// Filter habits based on the day of the week and map through to add new properties
+		const updatedHabits = filteredHabits.map((habit) => {
+			const today = format(selectedDate, 'yyyy-MM-dd');
+			const history = habit.history || {};
+			console.log(habit.name);
+			console.log(history);
+			const historyItem = history[today];
+			const isCompleted =
+				historyItem && historyItem.actionPerformed === 'finished';
+			console.log(isCompleted);
+			console.log('*******************');
+			return {
+				...habit,
+				completed: isCompleted,
+				style: {
+					borderRadius: '10px',
+					backgroundColor: isCompleted ? 'gray' : 'white',
+				},
+			};
+		});
+		// console.log(updatedHabits);
+		setHabits(updatedHabits);
+		setLoading(false);
+	};
+
+	useEffect(() => {
+		// Fetch habits on component mount and when selectedDate changes
+		fetchHabits(selectedDate);
+	}, [selectedDate]); // Dependency array ensures it runs when selectedDate changes
+
+	// Function to handle date change
+	const handleDateChange = (date) => {
+		setSelectedDate(date);
+		// Fetch habits when date changes
+		fetchHabits(date);
+	};
+
+	return (
+		<div className='container mx-auto p-4'>
+			<DatePicker selected={selectedDate} onChange={handleDateChange} inline />
+			{loading ? (
+				<Spinner />
+			) : (
+				habits.map((habit) => (
+					<div
+						key={habit.id}
+						className={`card p-4 m-2 border rounded shadow ${
+							habit.completed ? 'bg-green-100' : 'bg-white'
+						}`}
+					>
+						<h3
+							className={`text-lg font-semibold ${
+								habit.completed ? 'line-through text-gray-500' : 'text-black'
+							}`}
+						>
+							{habit.name}
+						</h3>
+						<p
+							className={
+								habit.completed ? 'line-through text-gray-500' : 'text-black'
+							}
+						>
+							{habit.daysOfWeek}
+						</p>
+						<h4 className='mt-2 font-medium'>Selected Date:</h4>
+						<p>{selectedDate.toISOString()}</p>
+						{habit.history &&
+							Object.entries(habit.history).map(([date, historyItem]) => (
+								<div key={date} className='mt-2'>
+									<h4 className='font-medium'>
+										History for {historyItem.date}:
+									</h4>
+									<p>{historyItem.actionPerformed}</p>
+								</div>
+							))}
+						<input
+							type='checkbox'
+							checked={habit.completed}
+							className='mt-2'
+							readOnly
+							onClick={() => handleCheckboxChange(habit.id)}
+						/>
+					</div>
+				))
+			)}
+		</div>
+	);
+};
 
 export default Todos;
